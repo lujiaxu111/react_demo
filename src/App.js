a1ad9dcbe33a93b442bda8b6e0e5c30fc09034f4
徐禄家 <59001292+lujiaxu111@users.noreply.github.com>
2024-02-29 15:36:01 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import  Mysele  from './pages/demo/demo.tsx'
+function App() {
+  return (
+    <div className="App">
+      <Mysele></Mysele>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/demo/demo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function  Mysele(){
+ return (
+  <div>
+    666
+  </div>
+ )
+}
